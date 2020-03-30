f55ce44f1410934a0891d15d65b635553aeaac9c
austinb847 <austinb847@icloud.com>
2020-03-30 12:45:05 -0700

    add a loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
-  $("h1").click(function() {
+  var tags = ["h1", "p" , "img"];
+  
+  tags.forEach(function(tag) {
+    $(tag).click(function() {
+      if (tag === "h1") {
+        alert("This is a header");
+      } else if (tag === "p") {
+        alert("This is a paragraph!");
+      } else if (tag === "img") {
+        alert("This is an image!!!");
+      } else {
+        alert("This is a different tag!");
+      }
+    });
+  });
+
+
+
+  /* $("h1").click(function() {
     alert("This is a header.");
   });
 
@@ -9,5 +27,5 @@ $(document).ready(function() {
 
   $("img").click(function() {
     alert("This is an image.");
-  });
+  }); */
 });
